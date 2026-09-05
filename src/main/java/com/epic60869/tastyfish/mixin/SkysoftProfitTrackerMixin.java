b3027ec2fba899a60ab13f8e9fed10bda29c4f94
epic60869-hue <epic60869@gmail.com>
2026-09-05 17:11:26 +0100

    Fix mixin accessor package crash

--- src/main/java/com/epic60869/tastyfish/mixin/SkysoftProfitTrackerMixin.java
+++ src/main/java/com/epic60869/tastyfish/mixin/SkysoftProfitTrackerMixin.java
@@ -1,5 +1,6 @@
 package com.epic60869.tastyfish.mixin;
 
+import com.epic60869.tastyfish.SkysoftProfitTrackerAccessor;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.Final;
